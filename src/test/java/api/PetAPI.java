f1c3fd3c5b6018d2d4291b3fe8f7518afdd188c3
dolbi_digital <dolbilov@gmail.com>
2023-08-18 17:55:22 +0200

    Cleaned code

--- src/test/java/api/PetAPI.java
+++ src/test/java/api/PetAPI.java
@@ -5,12 +5,9 @@ import pojo.Pet;
 
 import static api.SpecBuilder.getRequestSpec;
 import static api.SpecBuilder.getResponseSpec;
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 
 public class PetAPI {
-    //String baseUri = "https://petstore.swagger.io/v2/pet/";
-    String petId = "6666666666666666666";
     public static Response post(Pet requestPet) {
         return given(getRequestSpec()).
                 body(requestPet).
